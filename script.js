dee93d54910a9641bec9fa96a16df9a6da9e385d
Vanishree <vani2079@gmail.com>
2023-06-24 15:46:50 +0530

    3rd commit

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 const addButtonEle=document.querySelector("#addButton");
 const cartItemsEle=document.querySelector("#cartItems");
+//const itemList =[];
+let counter=0;
+
+
 function items(){
+  const currentItem={
+    id: new Date().getTime(),
+    counter:0
+  };
+
+   if(counter==0){
     const newDiv=document.createElement("div");
     newDiv.className="cartItem";
     const image=document.createElement("img");
@@ -8,11 +18,17 @@ function items(){
         image.setAttribute("alt","Not found");
     const h5Ele=document.createElement("h5");
     h5Ele.textContent="Item 1";
+    //itemList.push(h5Ele.textContent);
+    
     const button1Ele=document.createElement("button");
     button1Ele.className="item";
     button1Ele.textContent="+";
-    const h5Ele2=document.createElement("h5");
-    h5Ele2.textContent="1";
+   
+    let h5Ele2=document.createElement("h5");
+    counter=counter+1;
+    h5Ele2.textContent=counter;
+    button1Ele.addEventListener("click",incrementItem,false);
+    
     const button2Ele=document.createElement("button");
     button2Ele.className="item";
     button2Ele.textContent="-";
@@ -20,10 +36,6 @@ function items(){
     removeButton.textContent="Remove";
     removeButton.className="buttonRemove";
 
-
-
-    // const para=document.createElement("p");
-    // para.textContent="vani";
     newDiv.appendChild(image);
     newDiv.appendChild(h5Ele);
     newDiv.appendChild(button1Ele);
@@ -31,7 +43,18 @@ function items(){
     newDiv.appendChild(button2Ele);
     newDiv.appendChild(removeButton);
     cartItemsEle.appendChild(newDiv);
-    
 
+    }
+else{
+  document.getElementById("h5Ele2").innerHTML=currentItem.counter;
+  // incrementItem(items);
 }
+}
+function incrementItem(currentItem){
+  currentItem.counter=counter++;
+ console.log(currentItem.counter);
+ // h5Ele2.textContent=currentItem.counter;
+ document.getElementById("h5Ele2").innerText=currentItem.counter;
+}
+
 addButtonEle.addEventListener("click",items,false);
